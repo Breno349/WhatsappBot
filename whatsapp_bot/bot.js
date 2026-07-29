1e2c6a23697b847825889186f61a4bcbc5e1fb9f
Breno <123127632+Breno349@users.noreply.github.com>
2026-07-29 00:25:36 -0300

    Rename reply functions to send for images and videos

--- whatsapp_bot/bot.js
+++ whatsapp_bot/bot.js
@@ -279,9 +279,11 @@ export async function startWA( tentativa = 0 ){
                     editMsg: async (txt) => await sock.sendMessage(m.key.remoteJid, { text: txt, edit: m.key }),
                     replyFig: async (buffer) => await sock.sendMessage(m.key.remoteJid, { sticker: { url: buffer } }, { quoted: m }),
                     replyImage: async (buffer,caption='',view=false) => await sock.sendMessage(m.key.remoteJid, {image: {url: buffer}, viewOnce: view, caption}, { quoted: m } ),
-                    replyImageToPrivate: async (buffer,caption='') => await sock.sendMessage(lid, {image: {url: buffer}, caption}, { quoted: m } ),
+                    sendImage: async (buffer,caption='',view=false) => await sock.sendMessage(m.key.remoteJid, {image: {url: buffer}, viewOnce: view, caption} ),
+                    sendImageTo: async (buffer,caption='') => await sock.sendMessage(lid, {image: {url: buffer}, caption}, { quoted: m } ),
                     replyVideo: async (buffer,caption='',view=false) => await sock.sendMessage(m.key.remoteJid, {video: {url: buffer}, viewOnce: view, caption}, { quoted: m } ),
-                    replyVideoToPrivate: async (buffer,caption='') => await sock.sendMessage(lid, {video: {url: buffer}, caption}, { quoted: m } ),
+                    sendVideo: async (buffer,caption='',view=false) => await sock.sendMessage(m.key.remoteJid, {video: {url: buffer}, viewOnce: view, caption} ),
+                    sendVideoTo: async (buffer,caption='') => await sock.sendMessage(lid, {video: {url: buffer}, caption}, { quoted: m } ),
                     waitForResponse: async (opcoes) => await waitForResponse(m.key.remoteJid, lid, opcoes),
                     replyReact: async (emoji) => await sock.sendMessage(m.key.remoteJid, { react: { text: emoji, key: m.key } }), // emoji vazio '' remove a reação
                     replyAudio: async (buffer, ptt=false) => await sock.sendMessage(m.key.remoteJid, { audio: { url: buffer }, mimetype: 'audio/mp4', ptt }, { quoted: m }),
